21270f63be70c528b296215afece9307950eb348
ArtemSmit21 <smarde2103@mail.ru>
2024-10-06 19:06:39 +0300

    Add files(CustomArrayList)

---
+++ CustomArrayList/Classes/CustomArrayList.java
@@ -0,0 +1,74 @@
+package Classes;
+
+import Interfaces.CustomMethods;
+
+public class CustomArrayList<A> implements CustomMethods<A> {
+    private Object[] array;
+    private int startIndex;
+    private int finishIndex;
+    private int capacity;
+
+    public CustomArrayList() {
+        array = new Object[10];
+        startIndex = 0;
+        finishIndex = 0;
+        capacity = 10;
+    }
+
+    private void resize() {
+        capacity *= 2;
+        Object[] newArray = new Object[capacity];
+        for (int i = startIndex; i < finishIndex; i++) {
+            newArray[i - startIndex] = array[i];
+        }
+        array = newArray;
+        startIndex = 0;
+        finishIndex = finishIndex - startIndex;
+    }
+
+    @Override
+    public void add(A element) {
+        try {
+            if (element == null) {
+                throw new NullPointerException();
+            } else {
+                if (finishIndex == capacity) {
+                    resize();
+                }
+                array[finishIndex] = element;
+                finishIndex++;
+            }
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Нельзя в качестве аргументов использовать null!!!");
+        }
+    }
+
+    @Override
+    public void remove(int index) {
+        try {
+            if (index < startIndex || index >= finishIndex) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else {
+                for (int i = index; i < finishIndex - 1; i++) {
+                    array[i] = array[i + 1];
+                }
+                finishIndex--;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayIndexOutOfBoundsException("Вы вышли за пределы динамического массива!!!");
+        }
+    }
+
+    @Override
+    public A get(int index) {
+        try {
+            if (index < startIndex || index >= finishIndex) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else {
+                return (A) array[index];
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ArrayIndexOutOfBoundsException("Вы вышли за пределы динамического массива!!!");
+        }
+    }
+}
