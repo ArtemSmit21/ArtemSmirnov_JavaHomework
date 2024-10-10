8d9afa8f6776c61bd2b90323d0b6de5a651b3212
ArtemSmit21 <smarde2103@mail.ru>
2024-10-10 17:27:19 +0300

    Add files via upload

---
+++ test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testSum() {
+        assertEquals(3, 1 + 1);
+    }
+}
