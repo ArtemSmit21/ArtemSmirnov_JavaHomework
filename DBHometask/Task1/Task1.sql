60e149228a7c8f0d0c1ddc14fa8e25279eadd846
ArtemSmit21 <smarde2103@mail.ru>
2024-11-22 20:57:06 +0300

    Update Task1.sql

--- DBHometask/Task1/Task1.sql
+++ DBHometask/Task1/Task1.sql
@@ -1,2 +1,2 @@
 SELECT COUNT(profile_id) AS count_without_posts FROM profile
-WHERE profile_id NOT IN (SELECT DISTINCT profile_id FROM post);
+WHERE profile_id NOT IN (SELECT DISTINCT profile_id FROM post)
