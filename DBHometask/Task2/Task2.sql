165d9e1839fac36a5a99524d8e1b41973addd915
ArtemSmit21 <smarde2103@mail.ru>
2024-11-22 20:53:51 +0300

    Create Task2.sql

---
+++ DBHometask/Task2/Task2.sql
@@ -0,0 +1,7 @@
+WITH helpTable (post_id, number_of_comments) AS (
+    SELECT post_id, COUNT(post_id) FROM comment
+    GROUP BY post_id
+)
+SELECT helpTable.post_id AS post_id FROM helpTable
+INNER JOIN post ON helpTable.post_id = post.post_id AND ASCII(post.title) >= 48 AND ASCII(post.title) <= 57 AND LENGTH(post.content) > 20
+ORDER BY post_id;
